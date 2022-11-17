94540ea6b3ead77e9459e6f2a76136654d761a1c
Ahmed Mohamed Omer Mahmoud (s123157) <mahmoud.ahmedmohamedomer@student.ap.be>
2022-11-17 19:56:22 +0100

    home page and exhibition is add

---
+++ src/pages/exhibitions/collections.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+export const collections = () => {
+  return (
+ 
+    <main>
+      <title>Exhibitions page</title>
+      <h1>Collections</h1>
+      <p>hier komt collections </p>
+      <Link to='/'>Home</Link>
+    </main>
+   
+  )
+}
+
+export default collections;
