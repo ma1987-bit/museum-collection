c072ca619ec2d39ea8a5fe57da40656750ef2414
Ahmed Mohamed Omer Mahmoud (s123157) <mahmoud.ahmedmohamedomer@student.ap.be>
2022-12-15 23:49:32 +0100

    data binnen gehald in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,74 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link ,graphql} from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
+import Button from '@mui/material/Button';
 
 
 
 
-const IndexPage = () => (
+
+const IndexPage = ({data:{wpPage: {homeFields}}}) => {
+  const image = getImage(homeFields.picture.localFile)
+  return (
   <Layout>
-    
-     <h1>home page komt hier</h1>
-
+     <h1>{homeFields.title}</h1>
+     <GatsbyImage
+            image={image}
+            alt={homeFields.picture.altText}
+          />     <div dangerouslySetInnerHTML={{
+              __html: homeFields.description,
+          }}/>
+         <Button target="__blank" variant="contained" href={homeFields.callToAction.url}>{homeFields.callToAction.title}</Button>
   </Layout>
-)
+  )
+}
 
 
 export default IndexPage
+
+ export const query = graphql `
+query{
+  wpPage(slug: {eq: "home"}) {
+    homeFields {
+      featuredCollections {
+        ... on WpCollection {
+          id
+          slug
+          collectionFields {
+            collectionName
+            constructionYear
+            curator
+            exhibitionCapacity
+            exhibitionFloor
+            billboard {
+              altText
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED)
+                }
+              }
+            }
+          }
+        }
+      }
+      callToAction {
+        title
+        url
+      }
+      title
+      picture {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+      description
+    }
+  }
+}
+
+`
